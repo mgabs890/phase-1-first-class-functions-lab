7aa8072d6ad72a9f059bb644913b0d1041b62f4d
Gabriella Delgado <m.gabriella.delgado@gmail.com>
2021-10-19 23:23:06 -0400

    commit tree 30343e992e8f476d92aa0baf44d9f42a6734fc1e

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multipliedFare){
+    return function(fare){
+        return multipliedFare*fare;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, returnDriverMethod){
+    return returnDriverMethod(drivers)
+}
+
+selectDifferentDrivers(['nick', 'gaby'], eturnFirstTwoDrivers)
+
